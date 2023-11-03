b14c47cf1a02ea3180a6e1e1b1415b5e601f557d
Manuel <mc@manuelcorpas.com>
2023-11-03 17:32:10 +0000

    code working

--- PYTHON/03-alt-allele-sample-count.py
+++ PYTHON/03-alt-allele-sample-count.py
@@ -4,9 +4,103 @@
 # It identifies and counts alternative alleles in the genotype, ignoring reference alleles.
 # The script then prints out the count of alternative alleles for each sample and variant 
 # position where at least one alternative allele is present
+# The script counts alternative alleles for those coordinates that are queried from the database
 
+
+import mariadb
+import glob, os
+from pathlib import Path
+import configparser
 import re
+import pysam
+import sys
 
+config   = configparser.ConfigParser()
+config.read_file(open(r'CONF/mariadb.conf'))
+
+try:
+    db = mariadb.connect(
+        host   = config.get('peru','host'),
+        user   = config.get('peru','user'     ),
+        passwd = config.get('peru','password' ),
+        db     = config.get('peru','database'))
+
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+
+db.autocommit = True
+cursor = db.cursor(dictionary=True)
+
+def count_alt_alleles(vcf_path, region):
+    """
+    Counts alternative alleles per sample in a specified region of a VCF file.
+    
+    Parameters:
+    - vcf_path: str, path to the VCF file
+    - region: str, specific region in format 'chr:start-end'
+    """
+    try:
+        # Open the VCF file
+        vcf_file = pysam.VariantFile(vcf_path)
+        
+        # Extract the samples
+        samples = vcf_file.header.samples
+        
+        # Dictionary to hold the counts of alternative alleles per sample
+        alt_allele_counts = {sample: 0 for sample in samples}
+        
+        # Fetch the variants in the specified region
+        for variant in vcf_file.fetch(region=region):
+            for sample, sample_data in variant.samples.items():
+                # Count alternative alleles
+                if sample_data.alleles:
+                    alt_alleles = [allele for allele in sample_data.alleles if allele != variant.ref]
+                    alt_allele_counts[sample] += len(alt_alleles)
+        
+        results = {}
+ 
+        # Output the results
+        for sample, count in alt_allele_counts.items():
+            parts = sample.split('-')
+            Population = parts[0]
+            Sample_no  = parts[1]
+            if not Population in results:
+                results[Population] = 0
+            results[Population] += count
+            #print(region,end='\t')       
+            #print(f"{Population}\t{Sample_no}: {count}")
+            
+        #for pop in results:
+        #    print(f"{region}\t{pop}\t{results[pop]}")
+        return results;
+        # Close the VCF file
+        vcf_file.close()
+        
+    except Exception as e:
+        print(f"An error occurred: {e}", file=sys.stderr)
+
+# Peru.joint150WG.vcf.gz
+vcf_path = "INPUT/VCF/Peru.joint150WG.vcf.gz"
+
+# Unknown clinical significance of high impact in protein coding regions:
+sql1 = "SELECT Chromosome,Chr_Start,Chr_End FROM 02_UP_VEP_EX_COMMON_VAR_1_CONSEQ WHERE IMPACT ='HIGH' AND CLIN_SIG = '-' AND BIOTYPE ='protein_coding'"
+
+cursor.execute(sql1)
+
+results = cursor.fetchall()
+for result in results:
+    Chromosome = result['Chromosome']
+    Chr_Start  = result['Chr_Start']
+    Chr_End    = result['Chr_End']
+    region     = str(Chromosome) + ':' + str(Chr_Start) + "-" + str(Chr_End)
+    #print(region,sep='\t')
+    variant = count_alt_alleles(vcf_path, region)
+    print (region,variant)
+    
+    
+'''
 def count_alternative_alleles(vcf_filename):
     with open(vcf_filename, 'r') as vcf_file:
         sample_names = []
@@ -37,5 +131,5 @@ def count_alternative_alleles(vcf_filename):
                     print(f"Sample '{sample_names[i]}' has {alt_allele_count} alternative alleles at position {columns[1]}.")
 
 # Replace 'your_file.vcf' with the name of your input VCF file
-count_alternative_alleles('your_file.vcf')
-
+count_alternative_alleles('INPUT/VCF/split_34.vcf')
+'''
